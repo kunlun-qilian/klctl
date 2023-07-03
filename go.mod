80b2293942b5a6e0f3ad6b4d215b432fe9e14f13
pippozq <zengqi0529@163.com>
2023-07-03 18:00:56 +0800

    update

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/kunlun-qilian/klctl
 go 1.17
 
 require (
-	github.com/deepmap/oapi-codegen v1.11.0
+	github.com/deepmap/oapi-codegen v1.13.0
 	github.com/fatih/color v1.13.0
 	github.com/ghodss/yaml v1.0.0
 	github.com/go-courier/enumeration v1.3.1
@@ -13,28 +13,31 @@ require (
 )
 
 require (
-	github.com/getkin/kin-openapi v0.94.0 // indirect
+	github.com/getkin/kin-openapi v0.117.0 // indirect
 	github.com/go-courier/codegen v1.1.2 // indirect
 	github.com/go-courier/x v0.0.10 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/swag v0.21.1 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/invopop/yaml v0.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
-	github.com/labstack/echo/v4 v4.7.2 // indirect
-	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/labstack/echo/v4 v4.10.2 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/perimeterx/marshmallow v1.1.4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasttemplate v1.2.1 // indirect
-	golang.org/x/crypto v0.0.0-20220513210258-46612604a0f9 // indirect
-	golang.org/x/mod v0.6.0-dev.0.20220106191415-9b9b3d81d5e3 // indirect
-	golang.org/x/net v0.0.0-20220513224357-95641704303c // indirect
-	golang.org/x/sys v0.0.0-20220513210249-45d2b4557a2a // indirect
-	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/tools v0.1.10 // indirect
-	golang.org/x/xerrors v0.0.0-20220411194840-2f41105eb62f // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/tools v0.9.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/generate/openapi_client.go
+++ internal/generate/openapi_client.go
@@ -1,58 +1,58 @@
 package generate
 
 import (
-    "fmt"
-    "github.com/deepmap/oapi-codegen/pkg/codegen"
-    "github.com/deepmap/oapi-codegen/pkg/util"
-    "io/fs"
-    "io/ioutil"
-    "os"
+	"fmt"
+	"github.com/deepmap/oapi-codegen/pkg/codegen"
+	"github.com/deepmap/oapi-codegen/pkg/util"
+	"io/fs"
+	"io/ioutil"
+	"os"
 )
 
 func createDir(path string) {
-    _, err := os.Stat(path)
-    if err != nil {
-        err = os.Mkdir(path, fs.ModePerm)
-        if err != nil {
-            panic(err)
-        }
-    }
+	_, err := os.Stat(path)
+	if err != nil {
+		err = os.Mkdir(path, fs.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 func clientDir(path, packageName string) string {
-    return fmt.Sprintf("%s/client_%s", path, packageName)
+	return fmt.Sprintf("%s/client_%s", path, packageName)
 }
 
 func packName(packageName string) string {
-    return fmt.Sprintf("client_%s", packageName)
+	return fmt.Sprintf("client_%s", packageName)
 }
 
 func GenerateOpenapiClient(packageName, path, url string) {
 
-    createDir(clientDir(path, packageName))
+	createDir(clientDir(path, packageName))
 
-    co := codegen.Configuration{
-        PackageName: packName(packageName),
-        Generate: codegen.GenerateOptions{
-            Client:       true,
-            Models:       true,
-            EmbeddedSpec: true,
-        },
-    }
+	co := codegen.Configuration{
+		PackageName: packName(packageName),
+		Generate: codegen.GenerateOptions{
+			Client:       true,
+			Models:       true,
+			EmbeddedSpec: true,
+		},
+	}
 
-    swagger, err := util.LoadSwagger(url)
-    if err != nil {
-        panic(err)
-    }
+	swagger, err := util.LoadSwagger(url)
+	if err != nil {
+		panic(err)
+	}
 
-    code, err := codegen.Generate(swagger, co)
-    if err != nil {
-        fmt.Println("err: ", err)
-        panic(err)
-    }
+	code, err := codegen.Generate(swagger, co)
+	if err != nil {
+		fmt.Println("err: ", err)
+		panic(err)
+	}
 
-    err = ioutil.WriteFile(fmt.Sprintf("%s/%s.go", clientDir(path, packageName), packName(packageName)), []byte(code), fs.ModePerm)
-    if err != nil {
-        panic(fmt.Sprintf("error writing generated code to file: %s", err))
-    }
+	err = ioutil.WriteFile(fmt.Sprintf("%s/%s.go", clientDir(path, packageName), packName(packageName)), []byte(code), fs.ModePerm)
+	if err != nil {
+		panic(fmt.Sprintf("error writing generated code to file: %s", err))
+	}
 }
